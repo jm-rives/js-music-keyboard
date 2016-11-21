3f0a7ade8fd503adcd71c9a646065e4dccd8b85e
jm-rives <joanna.rives@gmail.com>
2016-11-21 15:31:35 -0800

    INITIAL Commit

--- noise.js
+++ noise.js
@@ -1,3 +1,16 @@
+'use strict'
+
+console.log("Nothings happend yet.");
+
 $(document).ready( function() {
-  // your code here
+  console.log("Inside the first fxn, nothings happened yet either");
+
+  $('div.instrument > .note_c').click(function(){
+    console.log("Inside .click() fxn");
+    $('div.audio > cAudio')[0].play();
+    console.log("Did that thing happen? NOPE");
+  });
 });
+  // $('div.instrument > note c').click()function()) {
+    // alert("That thing you wanted to work WERKED!")
+  // })
